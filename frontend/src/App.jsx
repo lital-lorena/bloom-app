e0339f6bef379980c3402bb0569b26d3072bd408
lorena <herlor13@gmail.com>
2026-06-04 15:46:23 +0200

    feat: add React Router with Login and Register routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
-
 function App() {
   return (
-    <div>
-    
-      <Login />
-      <Register/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+      </Routes>
+      </BrowserRouter>
   )
 }
 
